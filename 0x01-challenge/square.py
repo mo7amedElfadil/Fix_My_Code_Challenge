21ce709a1761cbb538e93bf2f91027ee379963ab
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-14 01:00:41 +0200

    square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,13 +4,14 @@ Square class
 """
 
 
-class square():
+class Square():
     """ Documentation """
 
     width = 0
     height = 0
 
-    def __init__(self, *_, **kwargs):
+    def __init__(self, **kwargs):
+        """ Constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -24,12 +25,12 @@ class square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
