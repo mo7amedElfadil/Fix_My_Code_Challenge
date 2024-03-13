28484e399f8db3e33c3e54ee98f22ad272c8ac5b
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-14 01:07:48 +0200

    return heightr

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""
-Square class
-"""
+""" Square class """
 
 
 class Square():
-    """ Documentation """
-
+    """ Square class """
     width = 0
-    # height = 0
+    height = 0
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """ Constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """ Area of the Square """
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """PermiterOfMySquare
-        """
-        return (self.width * 4) # + (self.height * 2)
+        """Permiter of the Square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String representation """
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = Square(width=12)
+    """ executes when main """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
